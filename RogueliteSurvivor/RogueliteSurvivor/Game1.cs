12d74ef2d329672d79bf25d531527e55b77943e8
Micah Caunter <mcaunter12@gmail.com>
2023-02-09 22:46:25 -0500

    feat: move map rendering to a system

--- RogueliteSurvivor/RogueliteSurvivor/Game1.cs
+++ RogueliteSurvivor/RogueliteSurvivor/Game1.cs
@@ -20,14 +20,12 @@ namespace RogueliteSurvivor
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
-        private TiledMap map;
-        private Dictionary<int, TiledTileset> tilesets;
-
         const int scaleFactor = 3;
         private Matrix transformMatrix;
 
         private World world;
         private List<IUpdateSystem> updateSystems;
+        private List<IRenderSystem> renderSystems;
         private Entity player;
 
         private Dictionary<string, Texture2D> textures;
@@ -50,14 +48,14 @@ namespace RogueliteSurvivor
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            map = new TiledMap(Path.Combine(Content.RootDirectory, "Demo.tmx"));
-            tilesets = map.GetTiledTilesets(Content.RootDirectory + "/");
-
-            textures = new Dictionary<string, Texture2D>();
-            textures.Add("tiles", Content.Load<Texture2D>("Tiles"));
-            textures.Add("player", Content.Load<Texture2D>("Animated_Mage_Character"));
+            textures = new Dictionary<string, Texture2D>
+            {
+                { "tiles", Content.Load<Texture2D>("Tiles") },
+                { "player", Content.Load<Texture2D>("Animated_Mage_Character") }
+            };
 
             world = World.Create();
+            world.Create(new MapInfo(Path.Combine(Content.RootDirectory, "Demo.tmx"), Content.RootDirectory + "/"));
             player = world.Create(
                 new Player(),
                 new Position() { XY = new Vector2(50, 50) },
@@ -73,6 +71,11 @@ namespace RogueliteSurvivor
                 new AnimationSetSystem(world),
                 new AnimationUpdateSystem(world),
                 new MoveSystem(world),
+            };
+
+            renderSystems = new List<IRenderSystem>
+            {
+                new RenderMapSystem(world),
             };
         }
 
@@ -95,46 +98,9 @@ namespace RogueliteSurvivor
 
             _spriteBatch.Begin(samplerState: SamplerState.PointClamp, transformMatrix: transformMatrix);  // Set samplerState to null to work with high res assets
 
-            var tileLayers = map.Layers.Where(x => x.type == TiledLayerType.TileLayer);
-
-            foreach (var layer in tileLayers)
+            foreach(var system in renderSystems)
             {
-                for (var y = 0; y < layer.height; y++)
-                {
-                    for (var x = 0; x < layer.width; x++)
-                    {
-                        var index = (y * layer.width) + x; // Assuming the default render order is used which is from right to bottom
-                        var gid = layer.data[index]; // The tileset tile index
-                        var tileX = x * map.TileWidth;
-                        var tileY = y * map.TileHeight;
-
-                        // Gid 0 is used to tell there is no tile set
-                        if (gid == 0)
-                        {
-                            continue;
-                        }
-
-                        // Helper method to fetch the right TieldMapTileset instance
-                        // This is a connection object Tiled uses for linking the correct tileset to the gid value using the firstgid property
-                        var mapTileset = map.GetTiledMapTileset(gid);
-
-                        // Retrieve the actual tileset based on the firstgid property of the connection object we retrieved just now
-                        var tileset = tilesets[mapTileset.firstgid];
-
-                        // Use the connection object as well as the tileset to figure out the source rectangle
-                        var rect = map.GetSourceRect(mapTileset, tileset, gid);
-
-                        // Create destination and source rectangles
-                        var source = new Rectangle(rect.x, rect.y, rect.width, rect.height);
-                        var destination = new Rectangle(tileX, tileY, map.TileWidth, map.TileHeight);
-
-                        SpriteEffects effects = SpriteEffects.None;
-                        double rotation = 0f;
-
-                        // Render sprite at position tileX, tileY using the rect
-                        _spriteBatch.Draw(textures["tiles"], new Vector2(tileX, tileY), source, Color.White, (float)rotation, player.Get<Position>().XY, 1f, effects, 0);
-                    }
-                }
+                system.Render(gameTime, _spriteBatch, textures, player);
             }
 
             _spriteBatch.Draw(textures["player"], new Vector2(125, 75), player.Get<SpriteSheet>().SourceRectangle(player.Get<Animation>().CurrentFrame), Color.White, 0f, new Vector2(0, 4), 1f, SpriteEffects.None, 0);
