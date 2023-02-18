54ce0226a796a5942a107f703b76fdac778bbbe0
Micah Caunter <mcaunter12@gmail.com>
2023-02-18 11:07:25 -0500

    feat: display game time in top right

--- RogueliteSurvivor/RogueliteSurvivor/Systems/RenderHudSystem.cs
+++ RogueliteSurvivor/RogueliteSurvivor/Systems/RenderHudSystem.cs
@@ -24,6 +24,7 @@ namespace RogueliteSurvivor.Systems
         }
 
         static Vector2 HealthLocation = new Vector2(10, 10);
+        static Vector2 TimeLocation = new Vector2(-100, 10);
         const int Increment = 64;
 
         public void Render(GameTime gameTime, SpriteBatch spriteBatch, Dictionary<string, Texture2D> textures, Entity player, float totalElapsedTime)
@@ -71,6 +72,13 @@ namespace RogueliteSurvivor.Systems
 
                 counter++;
             });
+
+            spriteBatch.DrawString(
+                    fonts["Font"],
+                    string.Concat("Time: ", float.Round(totalElapsedTime, 2)),
+                    TimeLocation + Vector2.UnitX * (graphics.PreferredBackBufferWidth / 3),
+                    Color.White
+                );
         }
     }
 }
